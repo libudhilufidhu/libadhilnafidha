0d0026f3553b9102371d03d4c517752556386c23
libadhilnafidha <libasharhank@gmail.com>
2025-02-02 03:40:14 +0530

    #

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>// Hide the preloader and display the body after the window has fully loaded
+window.onload = function () {
+    document.getElementById('preloader').style.display = 'none'; // Hide preloader
+    document.body.style.display = 'block'; // Show the body content
+}
+</script>
