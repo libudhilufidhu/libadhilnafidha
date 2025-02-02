2c30586e2b4a144a8576e36be464a633347379fc
libadhilnafidha <libasharhank@gmail.com>
2025-02-02 09:25:19 +0530

    feat: update images and add loading animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-<script>// Hide the preloader and display the body after the window has fully loaded
-window.onload = function () {
-    document.getElementById('preloader').style.display = 'none'; // Hide preloader
-    document.body.style.display = 'block'; // Show the body content
-}
-</script>
+<script>
+        // When the window is loaded, hide the preloader
+        window.onload = function() {
+            document.body.classList.add('loaded');
+        };
+    </script>
